32d6f5cfeb2b2de3fd49346b13c8913c0f5a621c
huong2909 <huong>
2022-06-19 13:18:21 +0700

    thuchanh

---
+++ src/main/java/com/codegym/Md4ThCustomerManagementSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.codegym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Md4ThCustomerManagementSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Md4ThCustomerManagementSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/codegym/service/ICustomerService.java
@@ -0,0 +1,8 @@
+package com.codegym.service;
+
+import com.codegym.model.Customer;
+import com.codegym.service.IGeneralService;
+import com.codegym.model.Customer;
+
+public interface ICustomerService extends IGeneralService<Customer> {
+}
